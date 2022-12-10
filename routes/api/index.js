91687446eaad1147616f234c98fa0e22b1c0f0b0
José Leyva <jrleyva41096@gmail.com>
2022-12-10 03:13:08 -0500

    fixed routes again

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughRoutes');
 
-router.use('/courses', userRoutes);
-router.use('/students', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
