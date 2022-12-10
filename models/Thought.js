a3b68fec8561c3c8404558191d4159f5eb148b3a
José Leyva <jrleyva41096@gmail.com>
2022-12-09 21:27:26 -0500

    refactored student model to be Thought model

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thought_text: {
+      type: String,
+      required: true,
+      max_length: 280,
+      min_length: 1,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        return formatDate(date)
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id:false,
+  }
+);
+
+thoughtSchema.virtual('thoughtCount').get(function () {
+  return this.friends.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
