c63465ba1d15eb4cd204448429e5fa0f25b0846d
José Leyva <jrleyva41096@gmail.com>
2022-12-10 03:04:27 -0500

    fixed routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/courses', userRoutes);
+router.use('/students', thoughtRoutes);
 
 module.exports = router;
